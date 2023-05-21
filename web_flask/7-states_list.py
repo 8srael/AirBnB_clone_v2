27e9d0b49cebf05b8a870b6cc4d52ac14062e378
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-05-21 16:03:53 -0700

    Task 8, List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,7 +11,7 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def display_cities():
     all_states = list(storage.all(State).values())
     return render_template('7-states_list.html', states=all_states)
