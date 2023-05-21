a41d9861697a6dec1c03fa11dcbaecdde906a05a
KOUASSI Israel <kouassiisrael36@gmail.com>
2023-05-21 13:38:24 -0700

    Task 5, number template py file

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+""" Starts a Flask web app
+    Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB”
+        /c/<text>: display “C ” followed by the value of the text variable
+        /python/(<text>): display “Python ”,
+        followed by the value of the text variable
+        The default value of text is “is cool”
+        (replace underscore _ symbols with a space)
+        /number/<n>: display “n is a number” only if n is an integer
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace('_', ' ')
+    return "C %s" % text
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    text = text.replace('_', ' ')
+    return "Python {}".format(escape(text))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '%d is a number' % n
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
