ae24b750e16b51ce399adcc2b3d609a062efa636
8srael <kouassiisrael36@gmail.com>
2023-06-11 14:15:43 -0700

    Task 4, keep it clean updated

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -6,7 +6,7 @@ Fabric script that deletes out-of-date archives
 import os
 from fabric.api import run, env
 
-env.hosts = ['52.91.144.233', '54.208.166.31']
+env.hosts = ['54.167.147.167', '100.25.117.132']
 
 
 def do_clean(number=0):
@@ -18,15 +18,15 @@ def do_clean(number=0):
     if number < 0:
         return
     elif number == 0 or number == 1:
-        number = 1
+        number_to_keep = 1
     else:
-        number += 1
+        number_to_keep = number + 1
 
     path1 = '/data/web_static/releases'
     path2 = '/versions'
 
     with cd(path):
-        run("ls -1t | tail -n +{} | xargs rm -rf".format(number))
+        run("ls -1t | tail -n +{} | xargs rm -rf".format(number_to_keep))
 
     with cd(path2):
-        run("ls -1t | tail -n +{} | xargs rm -rf".format(number))
+        run("ls -1t | tail -n +{} | xargs rm -rf".format(number_to_keep))
